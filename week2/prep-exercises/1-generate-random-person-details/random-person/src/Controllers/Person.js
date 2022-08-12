0de5acd1c683460156e3e58e3e53ec0decc1a129
ysmnclsknnl <101097614+ysmnclsknnl@users.noreply.github.com>
2022-08-12 21:17:03 +0200

    phone is added

--- week2/prep-exercises/1-generate-random-person-details/random-person/src/Controllers/Person.js
+++ week2/prep-exercises/1-generate-random-person-details/random-person/src/Controllers/Person.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 const Person = ({ person }) => {
-  const { name, lastName, email } = person;
+  console.log("person rendered");
+  const { name, lastName, email, phoneNumber } = person;
   return (
     <>
       <li>Name: {name}</li>
       <li>Surname: {lastName}</li>
       <li>E-mail: {email}</li>
+      <li>Phone: {phoneNumber}</li>
     </>
   );
 };
