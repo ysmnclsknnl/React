178a4a9e9f9ad73b3b922c35e147cd20f14cf0a4
ysmnclsknnl <101097614+ysmnclsknnl@users.noreply.github.com>
2022-08-16 21:34:24 +0200

    API url is added

--- week2/project/ecommerce/src/components/CategoryList.js
+++ week2/project/ecommerce/src/components/CategoryList.js
@@ -1,31 +1,51 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import Category from "./Category";
+import fetchData from "../helpers/fetchData";
+import Error from "./Error";
+import Loading from "./Loading";
+import API_URL from "../constants";
 
 const CategoryList = ({ selectedCategory, onCategoryChange }) => {
   const [allCategories, SetAllCategories] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   const getCategories = async () => {
-    const res = await fetch("https://fakestoreapi.com/products/categories");
-    const data = await res.json();
-    SetAllCategories(data);
+    setLoading(true);
+
+    const categoryData = await fetchData(`${API_URL}/categories`);
+
+    if (!categoryData) {
+      setError(true);
+      setLoading(false);
+    }
+    setLoading(false);
+    SetAllCategories(categoryData);
   };
 
   useEffect(() => {
     getCategories();
   }, []);
 
+  if (error) {
+    return <Error text="Categories can be loaded" />;
+  }
+
   return (
-    <nav>
-      {allCategories.map((category, index) => (
-        <Category
-          catClicked={category === selectedCategory}
-          onChange={onCategoryChange}
-          key={index}
-          text={category}
-        ></Category>
-      ))}
-    </nav>
+    <>
+      {loading && <Loading />}
+      <nav>
+        {allCategories.map((category, index) => (
+          <Category
+            catClicked={category === selectedCategory}
+            onChange={onCategoryChange}
+            key={index}
+            text={category}
+          ></Category>
+        ))}
+      </nav>
+    </>
   );
 };
 
