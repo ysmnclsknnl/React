9a8b7471973ff70d61edf599b089f7960c0cd215
ysmnclsknnl <101097614+ysmnclsknnl@users.noreply.github.com>
2022-08-16 21:31:18 +0200

    use effect use state removed

--- week2/project/ecommerce/src/App.js
+++ week2/project/ecommerce/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Home from "./Pages/Home";
 import ProductDetail from "./Pages/ProductDetail";
+import NotFound from "./Pages/NotFound";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="product/:productId" element={<ProductDetail />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
