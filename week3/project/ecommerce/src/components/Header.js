d81d6c1b11df4b3ff228e4b3ded39e715d75ea29
ysmnclsknnl <101097614+ysmnclsknnl@users.noreply.github.com>
2022-08-29 10:55:20 +0200

    header component is created

---
+++ week3/project/ecommerce/src/components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "../App.css";
+
+export const Header = () => {
+  return (
+    <header className="App-header">
+      <h1>Products</h1>
+      <nav>
+        <NavLink className="nav-link" to="/">
+          Products
+        </NavLink>
+        <NavLink className="nav-link" to="/favorites">
+          Favorites
+        </NavLink>
+      </nav>
+    </header>
+  );
+};
