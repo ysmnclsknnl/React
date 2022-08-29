8239342ec0564a4fd694f5cda5d0ab61f9d3eea8
ysmnclsknnl <101097614+ysmnclsknnl@users.noreply.github.com>
2022-08-29 10:59:43 +0200

    header component is added

--- week3/project/ecommerce/src/App.js
+++ week3/project/ecommerce/src/App.js
@@ -1,24 +1,28 @@
 import "./App.css";
-import Home from "./Pages/Home";
-import ProductDetail from "./Pages/ProductDetail";
-import NotFound from "./Pages/NotFound";
-import Favorites from "./Pages/Favorites";
+import Home from "./routes/Home";
+import ProductDetail from "./routes/ProductDetail";
+import NotFound from "./routes/NotFound";
+import Favorites from "./routes/Favorites";
+import { Header } from "./components/Header";
 import { GlobalProvider } from "./GlobalContext";
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <GlobalProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/favorites" element={<Favorites />} />
-          <Route path="product/:productId" element={<ProductDetail />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Router>
-    </GlobalProvider>
+    <Router>
+      <GlobalProvider>
+        <div className="App">
+          <Header />
+
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/favorites" element={<Favorites />} />
+            <Route path="product/:productId" element={<ProductDetail />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </GlobalProvider>
+    </Router>
   );
 }
 
